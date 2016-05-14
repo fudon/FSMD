0bc7c7791bac706d18a199abd6cf5fd478bb78fe
fudon <fuart@qq.com>
2016-05-14 16:07:17 +0800

    二维码

--- ShareEconomy/MVC/ViewController/FSMoneyController.m
+++ ShareEconomy/MVC/ViewController/FSMoneyController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FSMoneyController.h"
+#import "FSImageLabelView.h"
+#import "FSQRController.h"
 
 @interface FSMoneyController ()
 
@@ -16,8 +18,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"银";
-    // Do any additional setup after loading the view.
+    self.title = @"百宝箱";
+    
+    NSArray *array = @[@"二维码",@"记账本",@"设备信息",@"蓝牙",@"利息计算器",@"贷款计算器",@"个税计算器"];
+    NSArray *picArray = @[@"a9m",@"a_4",@"a_n",@"ae6",@"myintegral",@"my_history",@"tootoodingdan"];
+    
+    CGFloat width = (WIDTHFC - 100) / 4;
+    WEAKSELF(this);
+    for (int x = 0; x < array.count; x ++) {
+        FSImageLabelView *imageView = [FSImageLabelView imageLabelViewWithFrame:CGRectMake(20 + (x % 4) * (width + 20), 74 + (x / 4) * (width + 45), width, width + 25) imageName:picArray[x] text:array[x]];
+        imageView.block = ^ (FSImageLabelView *bImageLabelView){
+            [this imageViewAction:bImageLabelView];
+        };
+        imageView.tag = TAGIMAGEVIEW + x;
+        [self.view addSubview:imageView];
+    }
+}
+
+- (void)imageViewAction:(FSImageLabelView *)imageLabelView
+{
+    NSInteger index = imageLabelView.tag - TAGIMAGEVIEW;
+    switch (index) {
+        case 0:
+        {
+            FSQRController *qrController = [[FSQRController alloc] init];
+            [self.navigationController pushViewController:qrController animated:YES];
+        }
+            break;
+          case 1:
+        {}
+            break;
+            case 2:
+        {}
+            break;
+            case 3:
+        {}
+            break;
+            case 4:
+        {}
+            break;
+            case 5:
+        {}
+            break;
+            case 6:
+        {}
+            break;
+            case 7:
+        {}
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
