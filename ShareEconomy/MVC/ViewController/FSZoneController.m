828e8988aaa1a3a8419462a9442762daae9b1dc9
fudon <fuart@qq.com>
2016-06-29 08:10:27 +0800

    清理缓存功能

--- ShareEconomy/MVC/ViewController/FSZoneController.m
+++ ShareEconomy/MVC/ViewController/FSZoneController.m
@@ -7,8 +7,9 @@
 //
 
 #import "FSZoneController.h"
+#import "SDImageCache.h"
 
-@interface FSZoneController ()
+@interface FSZoneController ()<UITableViewDataSource,UITableViewDelegate>
 
 @end
 
@@ -17,7 +18,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"行";
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, WIDTHFC, HEIGHTFC - 64) style:UITableViewStyleGrouped];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:tableView];
     // Do any additional setup after loading the view.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"infoCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = @"清除缓存";
+    cell.detailTextLabel.text = [self sdWebCacheSize];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+- (NSString *)sdWebCacheSize
+{
+    SDImageCache *cacheManager = [SDImageCache sharedImageCache];
+    NSUInteger size = cacheManager.getSize;
+    return [FuData kMGTUnit:size];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        SDImageCache *cacheManager = [SDImageCache sharedImageCache];
+        [cacheManager cleanDisk];
+        [cacheManager clearDisk];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [tableView deselectRowAtIndexPath:indexPath animated:YES];
+            [self showTitle:@"清除成功"];
+        });
+    });
 }
 
 - (void)didReceiveMemoryWarning {
